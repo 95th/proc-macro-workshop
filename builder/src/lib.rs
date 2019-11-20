5f03664de43688e035d01aeba64cee1b288d4422
Gurwinder Singh <vargwin@gmail.com>
2019-11-20 21:14:29 +0530

    Start Builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,91 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::parse_macro_input;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as syn::DeriveInput);
+    let ident = &ast.ident;
 
-    unimplemented!()
+    let builder_name = format!("{}Builder", ident);
+    let builder_ident = syn::Ident::new(&builder_name, ident.span());
+
+    let fields = if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { named, .. }),
+        ..
+    }) = &ast.data
+    {
+        named
+    } else {
+        unimplemented!();
+    };
+
+    let is_option = |ty: &syn::Type| -> bool {
+        if let syn::Type::Path(p) = ty {
+            p.path.segments.len() == 1 && p.path.segments[0].ident == "Option"
+        } else {
+            false
+        }
+    };
+
+    let opt_fields = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        if is_option(&ty) {
+            quote! { #name: #ty }
+        } else {
+            quote! { #name: std::option::Option<#ty> }
+        }
+    });
+
+    let methods = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        quote! {
+            pub fn #name(&mut self, #name: #ty) -> &mut Self {
+                self.#name = Some(#name);
+                self
+            }
+        }
+    });
+
+    let builder_fields = fields.iter().map(|f| {
+        let name = &f.ident;
+        quote! { #name: None }
+    });
+
+    let build_fields = fields.iter().map(|f| {
+        let name = &f.ident;
+        quote! {
+            #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))?
+        }
+    });
+
+    let expanded = quote! {
+        pub struct #builder_ident {
+            #(#opt_fields,)*
+        }
+
+        impl #ident {
+            pub fn builder() -> #builder_ident {
+                #builder_ident {
+                    #(#builder_fields,)*
+                }
+            }
+        }
+
+        impl #builder_ident {
+            #(#methods)*
+
+            pub fn build(&mut self) -> Result<#ident, Box<dyn std::error::Error>> {
+                Ok(#ident {
+                    #(#build_fields,)*
+                })
+            }
+        }
+    };
+
+    expanded.into()
 }
