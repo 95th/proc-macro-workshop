a4cb11d19f9b562b483e44f9ebb24054da4942e8
Gurwinder Singh <vargwin@gmail.com>
2019-11-22 16:05:23 +0530

    Builder pass test#6 and start #7

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -4,7 +4,7 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::parse_macro_input;
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as syn::DeriveInput);
     let ident = &ast.ident;
@@ -22,18 +22,10 @@ pub fn derive(input: TokenStream) -> TokenStream {
         unimplemented!();
     };
 
-    let is_option = |ty: &syn::Type| -> bool {
-        if let syn::Type::Path(p) = ty {
-            p.path.segments.len() == 1 && p.path.segments[0].ident == "Option"
-        } else {
-            false
-        }
-    };
-
     let opt_fields = fields.iter().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
-        if is_option(&ty) {
+        if get_inner_ty("Option", ty).is_some() {
             quote! { #name: #ty }
         } else {
             quote! { #name: std::option::Option<#ty> }
@@ -43,12 +35,64 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let methods = fields.iter().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
-        quote! {
-            pub fn #name(&mut self, #name: #ty) -> &mut Self {
-                self.#name = Some(#name);
-                self
+        if let Some(inner_ty) = get_inner_ty("Option", ty) {
+            quote! {
+                pub fn #name(&mut self, #name: #inner_ty) -> &mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
+            }
+        } else {
+            quote! {
+                pub fn #name(&mut self, #name: #ty) -> &mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
             }
         }
+    });
+
+    let each_methods = fields.iter().filter_map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        for attr in &f.attrs {
+            if attr.path.segments.len() == 1 && attr.path.segments[0].ident == "builder" {
+                if let Some(proc_macro2::TokenTree::Group(g)) = attr.tokens.clone().into_iter().next() {
+                    let mut token = g.stream().into_iter();
+                    match token.next().unwrap() {
+                        proc_macro2::TokenTree::Ident(ident) => assert_eq!(ident, "each"),
+                        tt => panic!("Expected 'each', found {}", tt),
+                    }
+                    match token.next().unwrap() {
+                        proc_macro2::TokenTree::Punct(punct) => assert_eq!(punct.as_char(), '='),
+                        tt => panic!("Expected '=', found {}", tt),
+                    }
+                    let arg = match token.next().unwrap() {
+                        proc_macro2::TokenTree::Literal(lit) => lit,
+                        tt => panic!("Expected literal, found {}", tt),
+                    };
+                    match syn::Lit::new(arg) {
+                        syn::Lit::Str(s) => {
+                            let ident = syn::Ident::new(&s.value(), s.span());
+                            if let Some(inner_ty) = get_inner_ty("Vec", ty) {
+                                return Some(quote! {
+                                    pub fn #ident(&mut self, #ident: #inner_ty) -> &mut Self {
+                                        if let Some(v) = self.#name {
+                                            v.push(#ident);
+                                        } else {
+                                            self.#name = vec![#ident];
+                                        }
+                                        self
+                                    }
+                                });
+                            }
+                        },
+                        tt => panic!("Expected string, found {:?}", tt)
+                    }
+                }
+            }
+        }
+        None
     });
 
     let builder_fields = fields.iter().map(|f| {
@@ -58,8 +102,14 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let build_fields = fields.iter().map(|f| {
         let name = &f.ident;
-        quote! {
-            #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))?
+        if get_inner_ty("Option", &f.ty).is_some() {
+            quote! {
+                #name: self.#name.clone()
+            }
+        } else {
+            quote! {
+                #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))?
+            }
         }
     });
 
@@ -79,6 +129,8 @@ pub fn derive(input: TokenStream) -> TokenStream {
         impl #builder_ident {
             #(#methods)*
 
+            #(#each_methods)*
+
             pub fn build(&mut self) -> Result<#ident, Box<dyn std::error::Error>> {
                 Ok(#ident {
                     #(#build_fields,)*
@@ -88,4 +140,27 @@ pub fn derive(input: TokenStream) -> TokenStream {
     };
 
     expanded.into()
+}
+
+fn get_inner_ty<'a>(outer: &str, ty: &'a syn::Type) -> Option<&'a syn::Type> {
+    if let syn::Type::Path(p) = ty {
+        if p.path.segments.len() != 1 || p.path.segments[0].ident != outer {
+            return None;
+        }
+
+        if let syn::PathArguments::AngleBracketed(inner_ty) = &p.path.segments[0].arguments {
+            if inner_ty.args.len() != 1 {
+                return None;
+            }
+
+            for arg in &inner_ty.args {
+                return if let syn::GenericArgument::Type(t) = arg {
+                    Some(t)
+                } else {
+                    None
+                };
+            }
+        }
+    }
+    None
 }
